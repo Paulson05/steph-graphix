374f8ab4d04d5e71f76f5cb9097fcc00515319bd
okofu <okofuonyebuchi@gmail.com>
2020-09-18 12:37:27 -0700

    intersection observer added

--- app.js
+++ app.js
@@ -148,3 +148,79 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
   modal.style.display = "none";
 }
+
+
+
+const header = document.querySelector("header");
+const sectionOne = document.querySelector(".home-intro");
+const faders = document.querySelectorAll(".fade-in");
+const sliders = document.querySelectorAll(".slide-in");
+
+const sectionOneOptions = {
+  rootMargin: "-200px 0px 0px 0px"
+};
+
+const sectionOneObserver = new IntersectionObserver(function(
+  entries,
+  sectionOneObserver
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      header.classList.add("nav-scrolled");
+    } else {
+      header.classList.remove("nav-scrolled");
+    }
+  });
+},
+sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -250px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function(
+  entries,
+  appearOnScroll
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
+
+sliders.forEach(slider => {
+  appearOnScroll.observe(slider);
+});
+const appearOptions2 = {
+  threshold:1,
+  rootMargin:"0px 0px -100px 0px"
+};
+
+const appearOnScroll2 = new IntersectionObserver(function(
+  entries, 
+appearOnScroll2
+){
+    entries.forEach(entry =>{
+         if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll2.unobserve(entry.target);
+    }  
+    })
+},
+appearOptions2);
+faders.forEach(fader =>{
+  appearOnScroll2.observe(fader);
+})
